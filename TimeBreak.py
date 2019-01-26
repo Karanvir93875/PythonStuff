c5f4b61572fb693bbba547030e25057ab53f74ed
Karanvir93875 <ksin3798@gmail.com>
2019-01-25 21:08:21 -0500

    Add files via upload

---
+++ TimeBreak.py
@@ -0,0 +1,12 @@
+import time
+import webbrowser
+
+num_breaks = 3
+breaks_count = 0
+
+print("This program began on " +time.ctime())
+while breaks_count < num_breaks:
+	time.sleep(10)
+	webbrowser.open( "https://www.youtube.com/watch?v=cf3lgs_fFnM")
+	breaks_count = breaks_count+1
+	
