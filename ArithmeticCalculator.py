b76aa7aa4687cab1edd109bb403d92572901dc14
Karanvir93875 <ksin3798@gmail.com>
2019-01-26 21:29:16 -0500

    Update ArithmeticCalculator.py

--- ArithmeticCalculator.py
+++ ArithmeticCalculator.py
@@ -1,4 +1,4 @@
-print('Karanvirs first calculator')
+print('*****Karanvirs first calculator*****')
 tool=input ('Press any key if you wish to use this calculator')
 tool=input ('Please select an operation (+,-,/,*) : ')
 print('You have chosen: ', tool)
@@ -6,16 +6,16 @@ num1=int(input("Please enter you first number: "))
 num2=int(input("Please enter your second number: ")) 
 
 if tool == '+':
-	print("You have chosen addition, thus your answer is: ", num1+num2)
+	print("You have chosen addition, thus your answer is: ", num1+num2,", Calc-you-later!")
 	
 elif tool == '-':
-		print("You have chosen subtraction, thus your answer is: ", num1-num2)
+		print("You have chosen subtraction, thus your answer is: ", num1-num2,", Calc-you-later!")
 		
 elif tool == '*':
-		print("You have chosen multiplication, thus your answer is: ", num1*num2)
+		print("You have chosen multiplication, thus your answer is: ", num1*num2,", Calc-you-later!")
 		
 elif tool == '/':
-		print("You have chosen division, thus your answer is: ", num1/num2)
+		print("You have chosen division, thus your answer is: ", num1/num2, ", Calc-you-later!")
 		
 else:
-	print("You have done something wrong!")
+	print("You have done something wrong!") 
