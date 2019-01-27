2670a25284f82c600233d95d6d90a8909595aa74
Karanvir93875 <ksin3798@gmail.com>
2019-01-26 21:26:13 -0500

    Add files via upload

---
+++ ArithmeticCalculator.py
@@ -0,0 +1,21 @@
+print('Karanvirs first calculator')
+tool=input ('Press any key if you wish to use this calculator')
+tool=input ('Please select an operation (+,-,/,*) : ')
+print('You have chosen: ', tool)
+num1=int(input("Please enter you first number: "))
+num2=int(input("Please enter your second number: ")) 
+
+if tool == '+':
+	print("You have chosen addition, thus your answer is: ", num1+num2)
+	
+elif tool == '-':
+		print("You have chosen subtraction, thus your answer is: ", num1-num2)
+		
+elif tool == '*':
+		print("You have chosen multiplication, thus your answer is: ", num1*num2)
+		
+elif tool == '/':
+		print("You have chosen division, thus your answer is: ", num1/num2)
+		
+else:
+	print("You have done something wrong!")
